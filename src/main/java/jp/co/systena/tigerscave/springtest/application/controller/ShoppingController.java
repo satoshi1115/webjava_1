d01b6ef3a640264e58eacfb1bfa04f554692e65e
Systena <Systena@DESKTOP-IHALL7F>
2019-11-18 12:26:45 +0900

    WebJava課題１_20191118

--- src/main/java/jp/co/systena/tigerscave/springtest/application/controller/ShoppingController.java
+++ src/main/java/jp/co/systena/tigerscave/springtest/application/controller/ShoppingController.java
@@ -3,10 +3,13 @@ package jp.co.systena.tigerscave.springtest.application.controller;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import jp.co.systena.tigerscave.springtest.application.model.Item;
+import jp.co.systena.tigerscave.springtest.application.model.ListForm;
 import jp.co.systena.tigerscave.springtest.application.service.ListService;
 
 @Controller
@@ -15,31 +18,46 @@ public class ShoppingController {
   @Autowired
   ListService listService;
 
-  @RequestMapping(value="list",method=RequestMethod.GET)
+  @RequestMapping(value = "/list", method = RequestMethod.GET)
   public ModelAndView list(ModelAndView mav) {
 
-    //商品一覧をlistservice クラスより取得しクラス内のメソッドを使用
+    // 商品一覧をlistservice クラスより取得しクラス内のメソッドを使用
     Map<Integer, Item> itemList = listService.getItemList();
 
-    //テンプレートに値を渡す内容を設定
-    //第一引数は「変数」、第二引数は実際に渡す「オブジェクト」
-   mav.addObject("itemList",itemList);
+    // テンプレートに値を渡す内容を設定
+    // 第一引数は「変数」、第二引数は実際に渡す「オブジェクト」
+    mav.addObject("itemList", itemList);
 
-   //テンプレートファイル名を入力
-   mav.setViewName("Listview");
+    // テンプレートファイル名を入力
+    mav.setViewName("Listview");
 
-return mav;
+    return mav;
 
   }
-@RequestMapping(value = "/cart", method = RequestMethod.GET)
-  public ModelAndView cart(ModelAndView mav) {
+
+  // 「value」に設定されたはURLへのファイルパス。
+  @RequestMapping(value = "/list", method = RequestMethod.POST)
+  // @Validatedにて入力制限を設けたクラスファイルを記載する。
+  /*
+   * BindingResult型を用いる事によって@Validatedの結果を分かるようにする。
+   * 尚、引数のBindingResultの順番は、「Form/BindingResult/Model」のようにFormの直後に来なければ エラーが発生してしまう。
+   */
+  public ModelAndView order(ModelAndView mav, @Validated ListForm listForm,
+      BindingResult bindingResult) {
+    // if文にてbindingResultクラスのgetAllErrorsメソッドを使用する。
+    if (bindingResult.getAllErrors().size() > 0) {
+      Map<Integer, Item> itemListMap = listService.getItemList();
+      mav.addObject("itemList", itemListMap);
+
+      // テンプレートファイル名を設定
+      mav.setViewName("ListView");
+
+      return mav;
+    }
 
 
 
-
-
-
-
-}
+    return mav;
+  }
 
 }
